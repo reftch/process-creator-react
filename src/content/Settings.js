4e74c77abbc40221bbb91d1fbd18d0b9a6d4d45d
Taras Chornyi <j.developer@hotmail.com>
2021-03-31 22:01:47 +0200

    Added initial content to the pages

--- src/content/Settings.js
+++ src/content/Settings.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <>
-      Settings page
-    </>
+    <div className="form">
+      <div className="title">Settings</div>
+      <div className="form-description">
+        <span className="form-description-icon wui-icon-help" />
+          These are Process Creator's workspaces settings for the projects location behavior.
+          These settings are unrelated to the process's definitions, parameters and
+          behavior. <a href="#/">Learn more.</a>
+      </div>
+    </div>  
   )
 }
 
