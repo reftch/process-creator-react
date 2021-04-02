c4053e1042ed2e22b2b4e01e9742b8644725b57c
Taras Chornyi <j.developer@hotmail.com>
2021-04-02 11:57:15 +0200

    Remove warnings

--- src/content/Workspaces.js
+++ src/content/Workspaces.js
@@ -49,7 +49,7 @@ export const Workspaces = () => {
         <span className="form-description-icon wui-icon-help" />
           These are Process Creator's workspaces settings for the projects location behavior.
           These settings are unrelated to the process's definitions, parameters and
-          behavior. <a href="https://reactjs.org/"  target="_blank">Learn more.</a>
+          behavior. <a href="https://reactjs.org/" >Learn more.</a>
       </div>
 
       <div className="widget">
